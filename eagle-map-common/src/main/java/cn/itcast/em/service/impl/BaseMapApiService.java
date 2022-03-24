ac855aa0ad9ff52d101114f6963fbc6b7090553c
张志君 <mail.zhijun.zhang@icloud.com>
2022-03-24 08:14:41 +0800

    增加请求地图服务商失败重试机制

--- eagle-map-common/src/main/java/cn/itcast/em/service/impl/BaseMapApiService.java
+++ eagle-map-common/src/main/java/cn/itcast/em/service/impl/BaseMapApiService.java
@@ -1,6 +1,7 @@
 package cn.itcast.em.service.impl;
 
 import cn.hutool.core.convert.Convert;
+import cn.hutool.core.util.StrUtil;
 import cn.hutool.http.Header;
 import cn.hutool.http.HttpRequest;
 import cn.hutool.http.HttpResponse;
@@ -8,16 +9,25 @@ import cn.hutool.http.Method;
 import cn.itcast.em.config.EagleConfig;
 import cn.itcast.em.service.Function;
 import cn.itcast.em.service.MapApiService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.retry.annotation.Backoff;
+import org.springframework.retry.annotation.Recover;
+import org.springframework.retry.annotation.Retryable;
 
 import java.util.Map;
 
+/**
+ * 通用http请求发送服务，如果需要修改默认实现，通过继承的方式覆盖此类中的方法即可
+ */
+@Slf4j
 public abstract class BaseMapApiService implements MapApiService {
 
     @Autowired
     private EagleConfig eagleConfig;
 
     @Override
+    @Retryable(value = RuntimeException.class, maxAttempts = 4, backoff = @Backoff(delay = 2000L, multiplier = 2))
     public <T> T execute(String url, Method method, Map<String, Object> param, Function<T, HttpResponse> function) {
         HttpRequest httpRequest;
         switch (method) {
@@ -55,8 +65,16 @@ public abstract class BaseMapApiService implements MapApiService {
             return function.callback(response);
         } catch (Exception e) {
             //出现错误，抛出异常进行重试
-            throw new RuntimeException(e);
+            String msg = StrUtil.format("Map service provider call failed! url = {}, method = {}, param = {}", url, method, param);
+            throw new RuntimeException(msg, e);
         }
+    }
+
+    @Recover //全部重试失败后执行
+    public HttpResponse recover(RuntimeException e) {
+        //重试全部失败的情况下记录日志
+        log.error(e.getMessage(), e);
+        return null; //返回默认
     }
 
     /**
